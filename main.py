df66e7dee5e076d6ef89544f9c37c5528badcaf8
John Brennan <brennanj414@gmail.com>
2022-01-21 19:10:00 -0700

    get threads working properly

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 #2. Setup pause/play
 #3. Setup long hold = discoverable
 
+
+
 import asyncio
+import threading, time
 from time import time, sleep
 from gpiozero import LED, Button
 
@@ -11,30 +14,36 @@ button = Button(4)
 discoverable = True
 
 def setup_button_listener():
-  print("setting up button listener")
-  button.wait_for_press()
-  t0 = time()
-  print("you pressed the button")
-  button.wait_for_release()
-  t1= time()
-  print("you released the button")
-  hold_time = t1 - t0
-  if hold_time > 2:
-    led.on()
-    print("alright, let's discover devices")
-  else:
-
-    if button.wait_for_press(timeout=0.6):
-      print("fast forward")
+  while True:
+    print("setting up button listener")
+    button.wait_for_press()
+    t0 = time()
+    print("you pressed the button")
+    button.wait_for_release()
+    t1= time()
+    print("you released the button")
+    hold_time = t1 - t0
+    if hold_time > 2:
+      led.on()
+      print("alright, let's discover devices")
     else:
-      print("play/pause")
 
+      if button.wait_for_press(timeout=0.6):
+        print("fast forward")
+      else:
+        print("play/pause")
 
+def throbbing_led():
+  while True: 
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
 
 def main():
+  thread = threading.Thread(target=throbbing_led)
+  thread.start()
   print("starting up...")
   setup_button_listener()
-
-
 
 main()
